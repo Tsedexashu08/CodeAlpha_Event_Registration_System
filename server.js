92348344d256ae8868f5d0159ad6020f49d172e5
Tsedexashu08 <ashutsedex@gmail.com>
2026-01-06 20:05:31 +0300

    Ststed making the UI

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+// const supabase = require("./supabaseClient");
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port : ${PORT}`);
+});
