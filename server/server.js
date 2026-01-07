57ac0dc5a3613eeaf54f29dd9089e872da11c3e1
Tsedexashu08 <ashutsedex@gmail.com>
2026-01-07 21:13:27 +0300

    separating cleint side & server cause ill be using react from this point😅

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const supabase = require("./supabaseClient");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const userRoutes = require("../routes/authRoutes.js");
+
+app.use('/users', userRoutes);
+
+
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port : ${PORT}`);
+});
